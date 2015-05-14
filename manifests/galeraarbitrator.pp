e73865ebbe9fc36d5478373fe9114a1fac48d784
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-05-14 10:52:53 +0800

    galeraarbitrator

---
+++ manifests/galeraarbitrator.pp
@@ -0,0 +1,33 @@
+#
+class galeraarbitrator (
+  $galera_nodes = undef,
+  $galera_group = undef,
+  $galera_options = undef,
+  $galera_log_file = undef,
+) {
+
+  $packages = [
+    'galera-arbitrator-3'
+  ]
+
+  package { $packages:
+    ensure => installed
+  }
+
+  file { '/etc/default/garb':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('ccgcloud/garb.erb'),
+    require => Package[$packages],
+  }
+
+  service { 'garb':
+    ensure     => 'running',
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => Package[$packages],
+  }
+}
