2c7649275b868959e4fa6377bf6d6fce225337e7
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-06-03 23:13:26 +0800

    some config from ceph nodes

--- manifests/ceph.pp
+++ manifests/ceph.pp
@@ -1,5 +1,5 @@
 #
-class ccgcloud::ceph (
+class ccgcloud::ceph::conf (
   $fsid                     = undef,
   $mon_initial_members      = undef,
   $mon_host                 = undef,
@@ -17,7 +17,47 @@ class ccgcloud::ceph (
     require => Package[$packages],
   }
 
+}
+
+
+class ccgcloud::ceph::node (
+
+  $absent_packages = [ 'network-manager',
+                       'resolvconf',
+                       'ufw', ]
+
+  $packages = [ 'ethtool',
+                'iotop',
+                'iptraf',
+                'strace',
+                'tcpdump',
+                'tree', ]
+
+  package { $absent_packages:
+    ensure  => absent,
+  }
+
+  package { $packages:
+    ensure  => present,
+  }
+
+  $serial_src="start on stopped rc or RUNLEVEL=[12345]\nstop on runlevel [!12345]\n\nrespawn\nexec /sbin/getty -L 115200 ttyS0 vt102\n"
+  file {'/etc/init/ttyS0.conf':
+    content => $serial_src,
+  }
+
   file {'/etc/network/interfaces':
     content => template('ccgcloud/ceph/interfaces.erb'),
+  }
+}
+
+
+class ccgcloud::ceph::cephdeploy {
+  $packages = [
+    'ceph-deploy'
+  ]
+
+  package { $packages:
+    ensure  => present
   }
 }
