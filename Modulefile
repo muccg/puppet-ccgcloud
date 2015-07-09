f758b9eb3ec718d07cdbc692fc32a2ffbbc0070c
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2015-07-09 16:48:12 +0800

    patch nova-network to allow for external gateway

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name         'muccg-ccgcloud'
-version      '4.0.12'
+version      '4.0.13'
 
 author       'Centre for Comparative Genomics'
 license      'GNU GPL Version 3'
--- manifests/nova.pp
+++ manifests/nova.pp
@@ -43,4 +43,14 @@ class ccgcloud::nova::conf (
     content => template('ccgcloud/nova/nova.conf.erb'),
   }
 
+  # local patch to the kilo nova-network code to allow us to specify
+  # a hardware gateway. review to see if necessary in liberty.
+  file { '/usr/lib/python2.7/dist-packages/nova/network/linux_net.py':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('ccgcloud/nova/linux_net.py.erb'),
+  }
+
 }
